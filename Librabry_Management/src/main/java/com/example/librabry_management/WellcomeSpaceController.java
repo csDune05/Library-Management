1783ecbacc30ced5b47a904bac250a0d2dbe379c
csDune05 <23021492@vnu.edu.vn>
2024-11-09 19:59:47 +0700

    add wellcome space

---
+++ Librabry_Management/src/main/java/com/example/librabry_management/WellcomeSpaceController.java
@@ -0,0 +1,22 @@
+package com.example.librabry_management;
+
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class WellcomeSpaceController {
+    @FXML
+    private Button HomeButton;
+
+    @FXML
+    private Button AboutUsButton;
+
+    @FXML
+    private Button SighUpButton;
+
+    @FXML
+    private Button LoginButton;
+
+    @FXML
+    private Button GetStartedButton;
+}
