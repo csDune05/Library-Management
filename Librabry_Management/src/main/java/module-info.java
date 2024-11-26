9d58baa336eac62a8f24a89aae6f85e99ffac5a8
duy <23021504@vnu.edu.vn>
2024-11-27 00:31:06 +0700

    update profile thay doi anh dai dien

--- Librabry_Management/src/main/java/module-info.java
+++ Librabry_Management/src/main/java/module-info.java
@@ -8,12 +8,12 @@ module com.example.librabry_management {
     requires com.dlsc.formsfx;
     requires org.kordamp.bootstrapfx.core;
     requires com.almasb.fxgl.all;
-    requires java.desktop;
     requires com.zaxxer.hikari;
 
     requires java.prefs;
     requires json;
     requires javafx.media;
+    requires javafx.graphics;
     opens com.example.Controller to javafx.fxml;
     opens com.example.librabry_management to javafx.fxml;
     exports com.example.librabry_management;
